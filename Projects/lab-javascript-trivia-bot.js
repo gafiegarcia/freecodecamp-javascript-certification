ee935582028f1e0a5d3d27ffb44daef127dec781
Gafie Garcia <234681522+gafiegarcia@users.noreply.github.com>
2026-03-12 19:31:29 +0700

    chore: initialize repo and add the first project

---
+++ Projects/lab-javascript-trivia-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "dadBot";
+const botLocation = "my desktop chair";
+const favoriteLanguage = "CSS";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know that new changes to HTML and " + favoriteLanguage + " spec kept coming in the past few years?";
+console.log(codingFact);
+
+codingFact = "Did you know that it could take years for a proposed " + favoriteLanguage + " spec to get implemented by popular browsers?";
+console.log(codingFact);
+
+codingFact = "Did you know that now you could nest " + favoriteLanguage + " rules inside another?";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
