bb41daceb139df59d79efc3af49a03c672c564b9
Gafie Garcia <234681522+gafiegarcia@users.noreply.github.com>
2026-03-12 20:55:25 +0700

    chore: add a new project 'Lab: Build a Sentence Maker'

---
+++ Projects/lab-build-a-sentence-maker.js
@@ -0,0 +1,19 @@
+let adjective = "nerdy";
+let noun = "programmer";
+let verb = "pushed";
+let place = "hIDEout";
+let adjective2 = "humongous";
+let noun2 = "bugs";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "clueless";
+noun = "new GitHub user";
+verb = "judged for posting a bad first issue";
+place = "not-so-forgiving world";
+adjective2 = "cursed beginner";
+noun2 = "coding tutorials like a maniac"
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
