c091cfd52ddc2522cfcd67c06e79b7770e177517
Gafie Garcia <234681522+gafiegarcia@users.noreply.github.com>
2026-03-17 05:22:54 +0700

    chore: add "Build a Golf Score Translator" lab project

---
+++ Projects/lab-build-a-golf-score-translator.js
@@ -0,0 +1,21 @@
+// since every condition returns a value, I could've cleaned up the code more by stripping all "else" and just use stacked ifs
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes === par - 1) {
+    return names[2];
+  } else if (strokes === par) {
+    return names[3];
+  } else if (strokes === par + 1) {
+    return names[4];
+  } else if (strokes === par + 2) {
+    return names[5];
+  } else if (strokes >= par + 3) {
+    return names[6];
+  }
+}
